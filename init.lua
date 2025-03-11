77b67df6794cac146f43744e418f6002adfa4dae
VikingShow <2678907515@qq.com>
2025-03-11 21:10:14 +0800

    push before web configure

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+set clipboard+=unnamedplus
+vim.g.clipboard = {
+	name = "wl-clipboard",
+	copy = {
+	    ["+"] = "wl-copy",
+	    ["*"] = "wl-copy",
+	},
+	paste = {
+	    ["+"] = "wl-paste",
+	    ["*"] = "wl-paste",
+	},
+	cache_enabled = 1,
+}
